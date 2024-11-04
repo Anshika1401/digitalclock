0b4e69b47fe5b2e4d8db77b13d6b10988e6b0dd0
Anshika1401 <157337317+Anshika1401@users.noreply.github.com>
2024-11-04 10:38:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DigitalClock from './digitalclock';
+import './digitalclock.css';
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <DigitalClock />
+    </div>
+  );
+};
+
+export default App;
+
+
